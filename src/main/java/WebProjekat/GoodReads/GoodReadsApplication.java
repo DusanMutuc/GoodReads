78b248d7ecf973178b61d6aef3d52c65f128bd2e
DusanMutuc <mutucdusan@gmail.com>
2023-04-19 20:55:47 +0200

    initial commit

---
+++ src/main/java/WebProjekat/GoodReads/GoodReadsApplication.java
@@ -0,0 +1,13 @@
+package WebProjekat.GoodReads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodReadsApplication.class, args);
+	}
+
+}
