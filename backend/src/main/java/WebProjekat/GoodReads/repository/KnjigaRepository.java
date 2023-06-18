d1e2f207962bae9d2ea00fc48a3abfdeec5f89db
Dikicd <damjand48@gmail.com>
2023-06-18 22:44:03 +0200

    popravljen

--- backend/src/main/java/WebProjekat/GoodReads/repository/KnjigaRepository.java
+++ backend/src/main/java/WebProjekat/GoodReads/repository/KnjigaRepository.java
@@ -1,10 +1,13 @@
 package WebProjekat.GoodReads.repository;
 
 import WebProjekat.GoodReads.entity.Knjiga;
+import WebProjekat.GoodReads.entity.Zanr;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface KnjigaRepository extends JpaRepository<Knjiga, Long> {
     public Knjiga findByIsbn(String isbn);
+    public Optional<Knjiga> findByNaslov(String naslov);
 }
